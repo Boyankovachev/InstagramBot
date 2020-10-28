d3482b7d99dcb0572a68f2b214b7d6be6fd47f84
Boyan Kovachev <boyankovachev40@gmail.com>
2020-10-28 21:14:05 +0200

    Follow users bug fixes

--- InstagramBot.py
+++ InstagramBot.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import TimeoutException
 
 import random
 
@@ -99,22 +100,36 @@ class InstagramBot:
         located on any home page
         :returns int value of ↑
         """
-        try:
-            following_num = WebDriverWait(self.driver, 4).until(
-                EC.presence_of_element_located((By.XPATH, """//*[@id="react-root"]/section/main/div/header/section/ul
-                 /li[3]/a/span"""))
-            )
-        except:
-            # Instagram ima 2ta varianta za butoni
-            # napravi go taka che vmesto sus try except
-            # tursi dali ediniq element go ima i taka
-            following_num = WebDriverWait(self.driver, 4).until(
-                EC.presence_of_element_located((By.XPATH, """/html/body/div[1]/section/main/div/header/section/ul/li[3]/span/span"""))
-            )
+        WebDriverWait(self.driver, 4).until(
+            EC.presence_of_element_located(
+                (By.XPATH, """/html/body/div[1]/section/main"""))
+        )
+        option1 = False
+        option2 = False
+        if len(self.driver.find_elements_by_xpath(
+                """//*[@id="react-root"]/section/main/div/header/section/ul/li[3]/a/span""")) > 0:
+            option1 = True
+        elif len(self.driver.find_elements_by_xpath(
+                """/html/body/div[1]/section/main/div/header/section/ul/li[3]/span/span""")) > 0:
+            option2 = True
+        if option1:
+            following_num = self.driver.find_element_by_xpath(
+                """//*[@id="react-root"]/section/main/div/header/section/ul/li[3]/a/span""")
+        elif option2:
+            following_num = self.driver.find_element_by_xpath(
+                """/html/body/div[1]/section/main/div/header/section/ul/li[3]/span/span""")
+        else:
+            return ""
         temp = " "
         temp = following_num.text
         temp = temp.replace(',', '')
+        temp = temp.replace('.', '')
+        if "k" in temp:
+            temp[len(temp)] = ""
         return int(temp)
+        # //*[@id="react-root"]/section/main/div/header/section/ul/li[3]/a/span
+        # /html/body/div[1]/section/main/div/header/section/ul/li[3]/span/span
+        # /html/body/div[1]/section/main/div/ul/li[3]/span/span
 
     def get_followers_number(self):
         """
@@ -122,22 +137,32 @@ class InstagramBot:
         located on any home page
         :returns int value of ↑
         """
-
-        try:
-            following_num = WebDriverWait(self.driver, 4).until(
-                EC.presence_of_element_located((By.XPATH, """//*[@id="react-root"]/section/main/div/header/section/ul
-                 /li[2]/a/span"""))
-            )
-        except:
-            # Instagram ima 2ta varianta za butoni
-            # napravi go taka che vmesto sus try except
-            # tursi dali ediniq element go ima i taka
-            following_num = WebDriverWait(self.driver, 4).until(
-                EC.presence_of_element_located((By.XPATH, """/html/body/div[1]/section/main/div/header/section/ul/li[2]/span/span"""))
-            )
+        WebDriverWait(self.driver, 4).until(
+            EC.presence_of_element_located(
+                (By.XPATH, """/html/body/div[1]/section/main"""))
+        )
+        option1 = False
+        option2 = False
+        if len(self.driver.find_elements_by_xpath(
+                """//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a/span""")) > 0:
+            option1 = True
+        elif len(self.driver.find_elements_by_xpath(
+                """/html/body/div[1]/section/main/div/header/section/ul/li[2]/span/span""")) > 0:
+            option2 = True
+        if option1:
+            following_num = self.driver.find_element_by_xpath(
+                """//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a/span""")
+        elif option2:
+            following_num = self.driver.find_element_by_xpath(
+                """/html/body/div[1]/section/main/div/header/section/ul/li[2]/span/span""")
+        else:
+            return ""
         temp = " "
         temp = following_num.text
         temp = temp.replace(',', '')
+        temp = temp.replace('.', '')
+        if "k" in temp:
+            temp[len(temp)] = ""
         return int(temp)
 
     def open_following_tab(self):
@@ -145,7 +170,7 @@ class InstagramBot:
         opens the following tab when
         located on any home page
         """
-        following_button = WebDriverWait(self.driver, 20).until(
+        following_button = WebDriverWait(self.driver, 5).until(
             EC.presence_of_element_located((By.XPATH, """//*[@id="react-root"]/section/main/div/header/section/ul
              /li[3]/a"""))
         )
@@ -223,15 +248,31 @@ class InstagramBot:
         located on any home page tab
         follow user
         """
-        try:
-            follow_button = WebDriverWait(self.driver, 5).until(
-                EC.presence_of_element_located((By.XPATH, """/html/body/div[1]/section/main/div/header/section/
-                div[1]/div[2]/div/div/div/span/span[1]/button"""))
-            )
-            follow_button.click()
-        except:
-            return 1
-        return 0
+        WebDriverWait(self.driver, 4).until(
+            EC.presence_of_element_located(
+                (By.XPATH, """/html/body/div[1]/section/main"""))
+        )
+        option1 = False
+        option2 = False
+        if len(self.driver.find_elements_by_xpath(
+                """/html/body/div[1]/section/main/div/header/section/div[1]/div[1]/div/div/button""")) > 0:
+            option1 = True
+        elif len(self.driver.find_elements_by_xpath(
+                """/html/body/div[1]/section/main/div/header/section/div[1]/div[1]/div/div/div/span/span[1]/button""")) > 0:
+            option2 = True
+        else:
+            return False
+        if option1:
+            self.driver.find_element_by_xpath(
+                """/html/body/div[1]/section/main/div/header/section/div[1]/div[1]/div/div/button""").click()
+            return True
+        elif option2:
+            self.driver.find_element_by_xpath(
+                """/html/body/div[1]/section/main/div/header/section/div[1]/div[1]/div/div/div/span/span[1]/button""").click()
+            return True
+        return False
+        # /html/body/div[1]/section/main/div/header/section/div[1]/div[1]/div/div/button
+        # /html/body/div[1]/section/main/div/header/section/div[1]/div[1]/div/div/div/span/span[1]/button
 
 
 class UnfollowUsers(InstagramBot):
@@ -323,20 +364,31 @@ class FollowUsers(InstagramBot):
                         y) + "]/a")
 
         self.navigate_to_home_page(username)
-        image = WebDriverWait(self.driver, 20).until(
-            EC.presence_of_element_located(
-                (By.XPATH, random.choice(choice_list)))
-        )
-        image.click()
+        try:
+            image = WebDriverWait(self.driver, 5).until(
+                EC.presence_of_element_located(
+                    (By.XPATH, random.choice(choice_list)))
+            )
+            image.click()
+        except TimeoutException:
+            choice_list = []
+            for x in range(1, 9):
+                for y in range(1, 4):
+                    choice_list.append(
+                        "/html/body/div[1]/section/main/div/div[2]/article/div[1]/div/div[" + str(x) + "]/div[" + str(
+                            y) + "]/a")
+            image = WebDriverWait(self.driver, 5).until(
+                EC.presence_of_element_located(
+                    (By.XPATH, random.choice(choice_list)))
+            )
+            image.click()
 
     def scroll(self):
         """
         when opened likes panel
         scroll some
         """
-        time.sleep(1)
-        # likes_panel = self.driver.find_element_by_xpath("""/html/body/div[5]/div/div/div[2]/div""")
-        likes_panel = WebDriverWait(self.driver, 20).until(
+        likes_panel = WebDriverWait(self.driver, 5).until(
             EC.presence_of_element_located(
                 (By.XPATH, """/html/body/div[5]/div/div/div[2]/div"""))
         )
@@ -349,109 +401,99 @@ class FollowUsers(InstagramBot):
                  False if not
         """
         if followers > 5000 or following > 2500:
-            print(1, end=": ")
+            # print(1, end=": ")
             return False
-        if following > 1000 and followers < following*0.5:
-            print(2, end=": ")
+        if following > 1000 and followers < following * 0.5:
+            # print(2, end=": ")
             return False
         if followers < 10 or following < 20:
-            print(3, end=": ")
+            # print(3, end=": ")
             return False
         flag = False
-        if float(followers) > following*1.5:
-            if followers < 100 and following < followers*4:
+        if float(followers) > following * 1.5:
+            if followers < 100 and following < followers * 4:
                 flag = True
             if 200 > followers >= 100:
-                if following < followers*2:
+                if following < followers * 2:
                     flag = True
             if not flag:
-                print(4, end=": ")
+                # print(4, end=": ")
                 return False
         return True
 
     def start_following(self, num_to_follow):
         """
-        on open post start following
+        once logged in,
+        open post and start following
         people with the checks and all...
         """
         num_followed = 0
+        users_followed = []
 
-        likes_button = WebDriverWait(self.driver, 20).until(
-            EC.presence_of_element_located(
-                (By.XPATH, """/html/body/div[4]/div[2]/div/article/div[3]/section[2]/div/div[2]/button"""))
-        )
-        likes_button.click()
+        while num_followed < num_to_follow:
+            # opens a random post
+            self.open_random_post(self.get_random_user())
 
-        #wait to open before scraping
-        WebDriverWait(self.driver, 20).until(
-            EC.presence_of_element_located(
-                (By.XPATH, """/html/body/div[4]/div[2]/div/article/div[3]/section[1]/span[1]/button"""))
-        )
+            # open likes panel
+            likes_button = WebDriverWait(self.driver, 5).until(
+                EC.presence_of_element_located(
+                    (By.XPATH, """/html/body/div[4]/div[2]/div/article/div[3]/section[2]/div/div[2]/button"""))
+            )
+            likes_button.click()
+            # !!!!!!
+            # ponqkoga nqma laikove ako e video fix sus sledene na element
+            # /html/body/div[4]/div[2]/div/article/div[3]/section[2]/div/div[2]/button
 
-        users_list = []
-        users_scraped = 0
-        temp = 1
-        while users_scraped < num_to_follow:
-            try:
-                user_element = WebDriverWait(self.driver, 1).until(
-                    EC.presence_of_element_located(
-                        (By.XPATH, """/html/body/div[5]/div/div/div[2]/div/div/div[""" + str(
-                            temp) + """]/div[2]/div[1]/div/span/a"""))
-                )
-                user = user_element.text
-                if user not in users_list:
-                    users_list.append(user)
-                    users_scraped = users_scraped + 1
-                temp = temp + 1
-            except:
-                temp = 1
-                self.scroll()
-                continue
+            # wait to open before scraping
+            WebDriverWait(self.driver, 5).until(
+                EC.presence_of_element_located(
+                    (By.XPATH, """/html/body/div[4]/div[2]/div/article/div[3]/section[1]/span[1]/button"""))
+            )
 
-        for user in users_list:
-            self.navigate_to_home_page(user)
-            if self.is_eligible_for_follow(self.get_followers_number(), self.get_following_number()):
-                #if self.follow_user():
-                #    num_followed = num_followed + 1
-                print(str(self.get_followers_number()) + " / " + str(self.get_following_number()) + " DA")
-            else:
-                print(str(self.get_followers_number()) + " / " + str(self.get_following_number()) + " NE")
-            #time.sleep(7)
+            users_list = []
+            users_scraped = 0
+            temp = 1
+            while users_scraped < num_to_follow:
+                try:
+                    user_element = WebDriverWait(self.driver, 1).until(
+                        EC.presence_of_element_located(
+                            (By.XPATH, """/html/body/div[5]/div/div/div[2]/div/div/div[""" + str(
+                                temp) + """]/div[2]/div[1]/div/span/a"""))
+                    )
+                    user = user_element.text
+                    if user not in users_list:
+                        users_list.append(user)
+                        users_scraped = users_scraped + 1
+                    temp = temp + 1
+                except:
+                    temp = 1
+                    self.scroll()
+                    continue
 
+            for user in users_list:
+                self.navigate_to_home_page(user)
+                if self.is_eligible_for_follow(self.get_followers_number(), self.get_following_number()):
+                    if self.follow_user():
+                        num_followed = num_followed + 1
+                        users_followed.append(user)
+                        # tuka logvash koi e follownal
 
-# /html/body/div[5]/div/div/div[2]/div/div/div[1]/div[2]/div[1]/div/span/a
-# /html/body/div[5]/div/div/div[2]/div/div/div[2]/div[2]/div[1]/div/span/a
+        print(users_followed)
+
 """
 instagram = UnfollowUsers(*read_credentials())
 instagram.start_unfollowing(5, 500)
 instagram.driver.quit()
 """
+
 instagram = FollowUsers(*read_credentials())
 instagram.login()
-instagram.open_random_post(instagram.get_random_user())
-instagram.start_following(10)
+instagram.start_following(5)
 instagram.driver.quit()
+
 """
 1vi probelm - kato scrolva se bugva i ne zarejda sledvashtite
 2ri problem - kato scrolva spira da zarejda sledvashtite
 3ti problem - ponqkoga prosto skrolva do dolu i nishto ne prai
 (reshenie: dobavi except, koito gi hvashtat logvat gi i produljavat)
 """
-
-# like buton xpath
-# /html/body/div[4]/div[2]/div/article/div[3]/section[2]/div/div[2]/button
-
-
-# /html/body/div[5]/div/div/div[2]/div/div/div[1]/div[2]/div/div/span/a
-# /html/body/div[5]/div/div/div[2]/div/div/div[2]/div[2]/div[1]/div/span/a
-# /html/body/div[5]/div/div/div[2]/div/div/div[3]/div[2]/div[1]/div/span/a
-# /html/body/div[5]/div/div/div[2]/div/div/div[4]/div[2]/div[1]/div/span/a
-# /html/body/div[5]/div/div/div[2]/div/div/div[8]/div[2]/div[1]/div/span/a
-# /html/body/div[5]/div/div/div[2]/div/div/div[9]/div[2]/div[1]/div/span/a
-# /html/body/div[5]/div/div/div[2]/div/div/div[9]/div[2]/div[1]/div/span/a
-
-
-# /html/body/div[4]/div/div/div[2]/ul/div/li[1]/div/div[1]/div[2]/div[1]/span/a
-# /html/body/div[4]/div/div/div[2]/ul/div/li[2]/div/div[1]/div[2]/div[1]/span/a
-# /html/body/div[4]/div/div/div[2]/ul/div/li[3]/div/div[1]/div[2]/div[1]/span/a
-# /html/body/div[4]/div/div/div[2]/ul/div/li[166]/div/div[1]/div[2]/div[1]/span/a
